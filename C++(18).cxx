d889cdaca1c7815495e8a49dc85b6b30a095aa7e
parisa zafari <73137848+parisa797@users.noreply.github.com>
2023-09-08 01:30:56 +0330

    Add files via upload

---
+++ C++(18).cxx
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespsce std;
+int main()
+int count=0;
+num=0;
+T=0;
+bool found=True;
+int arry[20];
+for(int i=0;i<20;i++)
+arry[i]=0;
+while(count!=20)
+{
+	cin>>num;
+	if(num>988;num<101)
+	{
+		count++;
+		for(int i=0;i<count;i++)
+		{
+			if(arry[i]==num)
+			found=false;
+		}
+		if(found){
+			arry[T]=num;
+			T++;
+		}
+		endl;
