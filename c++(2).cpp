7b8ff765e8d92cf2664ac6a7898d368ff8c6dc52
parisa zafari <73137848+parisa797@users.noreply.github.com>
2023-09-08 00:50:13 +0330

    Add files via upload

---
+++ c++(2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double shoaa ,ghotr , masahat , mohit;
+    cout << "shoaa ra vared konid";
+    cin >>  shoaa;
+  ghotr= 2 *  shoaa;
+  cout << "ghotr=" << ghotr << endl;
+ masahat= 3.14 * shoaa * shoaa;
+ cout <<"masahat="<< masahat << endl;
+    mohit = 2 * 3.14 * shoaa;
+    cout << "mohit="<< mohit << endl;
+   
+}
+
+ 
